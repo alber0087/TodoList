5613bdd4296e427f4ee24d5bdf1d8f412c0527c9
Alber <albertorg0087@gmail.com>
2022-05-02 19:27:12 +0100

    Updates

---
+++ precache-manifest.32a0c1396bf989570137a9555d442c3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1d2597b358c2c5fa7de",
+    "url": "/todolist/static/css/main.399afe72.chunk.css"
+  },
+  {
+    "revision": "f1d2597b358c2c5fa7de",
+    "url": "/todolist/static/js/main.832f9e5e.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  },
+  {
+    "revision": "7566db366df36217c248",
+    "url": "/todolist/static/css/2.90207de3.chunk.css"
+  },
+  {
+    "revision": "7566db366df36217c248",
+    "url": "/todolist/static/js/2.1a9b6ed4.chunk.js"
+  },
+  {
+    "revision": "6b00037d45450e9157df741d9a649ef4",
+    "url": "/todolist/index.html"
+  }
+];
